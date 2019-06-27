42072ccbb59e462844aafb9ee5f324afa075b5cf
Erawat Chamanont <erawat@gmail.com>
2019-06-27 18:10:09 +0100

    RSE-210: Using constants array for declare settings

--- CRM/EventsExtras/SettingsManager.php
+++ CRM/EventsExtras/SettingsManager.php
@@ -12,36 +12,34 @@ class CRM_EventsExtras_SettingsManager {
   const EVENT_FEE = 'event_fee';
   const EVENT_REGISTRATION = 'event_online_registration';
 
-    /**
-    * Constants value for settings name
-    */
-   const SETTING_FIELDS = [
-    'eventsextras_roles',
-    'eventsextras_roles_default',
-    'eventsextras_participant_listing',
-    'eventsextras_participant_listing_default',
-    'eventsextras_event_summary',
-    'eventsextras_event_description',
-    'eventsextras_include_map_event_location',
-    'eventsextras_include_map_event_location_default',
-    'eventsextras_public_event',
-    'eventsextras_public_event_default',
-    'eventsextras_currency',
-    'eventsextras_currency_default',
-    'eventsextras_payment_processor_selection',
-    'eventsextras_payment_processor_selection_default',
-    'eventsextras_enable_pay_later_option',
-    'eventsextras_enable_pay_later_option_default',
-    'eventsextras_enable_pay_later_option_default_label',
-    'eventsextras_enable_pay_later_option_default_instruction',
-    'eventsextras_enable_pay_later_option_default_billing_address_required',
-    'eventsextras_pending_participant_expiration',
-    'eventsextras_pending_participant_expiration_default',
-    'eventsextras_allow_self_service',
-    'eventsextras_allow_self_service_default',
-    'eventsextras_allow_self_service_default_time_limit',
-    'eventsextras_register_multiple_participants',
-    'eventsextras_register_multiple_participants_default_maxinum_participant'
+
+  const SETTING_FIELDS = [
+   'ROLES' => 'eventsextras_roles',
+   'ROLES_DEFAULT' => 'eventsextras_roles_default',
+   'PARTICIPANT_LISTING' => 'eventsextras_participant_listing',
+   'PARTICIPANT_LISTING_DEFAULT' => 'eventsextras_participant_listing_default',
+   'PARTICIPANT_SUMMARY' => 'eventsextras_event_summary',
+   'EVENT_DESCRTIPTION' => 'eventsextras_event_description',
+   'INCLUDE_MAP_LOCATION_EVENT' => 'eventsextras_include_map_event_location',
+   'INCLUDE_MAP_LOCATION_EVENT_DEFAULT' => 'eventsextras_include_map_event_location_default',
+   'INCLUDE_MAP_PUBLIC_EVENT' => 'eventsextras_public_event',
+   'INCLUDE_MAP_PUBLIC_EVENT_DEFAULT' => 'eventsextras_public_event_default',
+   'CURRENCY' => 'eventsextras_currency',
+   'CURRENCY_DEFAULT' => 'eventsextras_currency_default',
+   'PAYMENT_PROCESSOR_SELECTION' => 'eventsextras_payment_processor_selection',
+   'PAYMENT_PROCESSOR_SELECTION_DEFAULT' => 'eventsextras_payment_processor_selection_default',
+   'PAY_LATER_OPTION' => 'eventsextras_enable_pay_later_option',
+   'PAY_LATER_OPTION_DEFAULT' => 'eventsextras_enable_pay_later_option_default',
+   'PAY_LATER_OPTION_DEFAULT_LABEL' => 'eventsextras_enable_pay_later_option_default_label',
+   'PAY_LATER_OPTION_DEFAULT_LABEL_INSTRUCTION' => 'eventsextras_enable_pay_later_option_default_instruction',
+   'PAY_LATER_OPTION_DEFAULT_BILLING_ADDRESS' => 'eventsextras_enable_pay_later_option_default_billing_address_required',
+   'PENDING_PARTICIPANT_EXPIRATION' => 'eventsextras_pending_participant_expiration',
+   'PENDING_PARTICIPANT_EXPIRATION_DEFAULT' => 'eventsextras_pending_participant_expiration_default',
+   'ALLOW_SELF_SERIVCE' => 'eventsextras_allow_self_service',
+   'ALLOW_SELF_SERVICE_DEFAULT' => 'eventsextras_allow_self_service_default',
+   'ALLOW_SELF_SERVICE_DEFAULT_TIME_LIMIT' => 'eventsextras_allow_self_service_default_time_limit',
+   'REGISTER_MULTIPLE_PARTICIPANTS' => 'eventsextras_register_multiple_participants',
+   'REGISTER_MULTIPLE_PARTICIPANTS_DEFAULT' => 'eventsextras_register_multiple_participants_default_maxinum_participant'
   ];
 
    /**
@@ -61,9 +59,12 @@ class CRM_EventsExtras_SettingsManager {
    *
    * @return array
   */
-  public static function getSettingsValue(){
+  public static function getSettingsValue($settings = NULL){
+    if ($settings == NULL){
+      $settings = array_values(self::SETTING_FIELDS);
+    }
     return civicrm_api3('setting', 'get', [
-      'return' => self::SETTING_FIELDS,
+      'return' => $settings,
       'sequential' => 1,
     ])['values'][0];
   }
@@ -99,7 +100,7 @@ class CRM_EventsExtras_SettingsManager {
    */
   private static function fetchSettingFields() {
     $settingFields = [];
-    foreach (self::SETTING_FIELDS  as $name) {
+    foreach (array_values(self::SETTING_FIELDS)  as $name) {
         $settingFields[$name] = civicrm_api3('setting', 'getfields',[
             'filters' => [ 'name' => $name],
         ])['values'][$name];
