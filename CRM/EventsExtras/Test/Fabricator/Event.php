47c29046533067b21da870e2ce46f4f0d487e7de
Ahed <ahed@compucorp.co.uk>
2020-12-16 15:53:19 +0200

    MAE-398: Fix start date to be in the future for EventFabricator

--- CRM/EventsExtras/Test/Fabricator/Event.php
+++ CRM/EventsExtras/Test/Fabricator/Event.php
@@ -21,7 +21,27 @@ class CRM_EventsExtras_Test_Fabricator_Event extends BaseFabricator {
   protected static $defaultParams = [
     'title'  => 'Event Sample' ,
     'event_type_id'  => 3 ,
-    'start_date'  => 20201201 ,
   ];
+
+
+  /**
+   * Fabricates an event  with the given parameters.
+   *
+   * @param array $params
+   *
+   * @return array
+   * @throws \CiviCRM_API3_Exception
+   */
+  public static function fabricate(array $params = []) {
+    // start_date should always be the future
+    $start_date = new DateTime('tomorrow');
+
+    $defaultParams = array_merge(static::$defaultParams, [
+      'start_date' => $start_date->format('Ymd'),
+    ]);
+    $params = array_merge($defaultParams, $params);
+
+    return parent::fabricate($params);
+  }
 
 }
