37256dfeb9028ac698a7ca25302d3b0fb4dcedf8
Ahed <ahed@compucorp.co.uk>
2020-12-28 12:20:51 +0200

    MAE-422: Create upgrader to reset the menu cache

--- CRM/EventsExtras/Upgrader.php
+++ CRM/EventsExtras/Upgrader.php
@@ -12,4 +12,18 @@ class CRM_EventsExtras_Upgrader extends CRM_EventsExtras_Upgrader_Base {
     $this->executeSqlFile('sql/auto_uninstall.sql');
   }
 
+  public function upgrade_0001() {
+    $this->resetMenuCache();
+
+    return TRUE;
+  }
+
+  /**
+   * After modifying the navigation menu we should reset the menu cache
+   */
+  private function resetMenuCache() {
+    $params = [];
+    civicrm_api3('System', 'flush', $params);
+  }
+
 }
